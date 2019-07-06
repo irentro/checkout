ccf192b5870e8775004597794f20658c7b395d3f
cnsarmi <christiannsarmiento@gmail.com>
2019-07-05 21:01:06 -0700

    Created webpack.config.js folder for webpack environment set up

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  watch: true,
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  }
+};
