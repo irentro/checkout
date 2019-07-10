48fe52e257dc00961505198a50c2e7c71718a278
cnsarmi <christiannsarmiento@gmail.com>
2019-07-10 12:35:44 -0700

    Created my Guests component

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import Guests from './components/Guests.jsx'
+import axios from 'axios';
 
 class Checkout extends React.Component {
     constructor(props) {
         super(props);
         
+        this.state = {
+            listing: {}
+        }
+    }
+
+    componentDidMount() {
+        axios.get('/listings')
+        .then(res => {
+            console.log(res.data[13])
+            this.setState({
+                listing: res.data[13],
+                price: res.data[13].per_night_price.slice(0, -3)
+            })
+        })
     }
 
     render() {
+
         return (
             <div className="checkout-container">
-                <div className="price">$1,000</div>
+                <div className="price">${this.state.price}</div>
                 <p className="per-night">per night</p>
 
                 <div className="stars-container">
@@ -20,7 +37,7 @@ class Checkout extends React.Component {
                         <svg className="stars" viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='#008489'><path d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/></svg>
                         <svg className="stars" viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg' fill='#008489'><path d='M972 380c9 28 2 50-20 67L725 619l87 280c11 39-18 75-54 75-12 0-23-4-33-12L499 790 273 962a58 58 0 0 1-78-12 50 50 0 0 1-8-51l86-278L46 447c-21-17-28-39-19-67 8-24 29-40 52-40h280l87-279c7-23 28-39 52-39 25 0 47 17 54 41l87 277h280c24 0 45 16 53 40z'/></svg>
                         <div className="review-count">
-                        <p className="reviews">69</p>
+                        <p className="reviews">127</p>
                     </div>
                     </div>
                 </div>
@@ -55,24 +72,7 @@ class Checkout extends React.Component {
                         </div>
                     </div>
 
-                    <div className="guest-component">
-                        <label className="guest-label">
-                            <span className="guest">Guests</span>
-                        </label>
-                        <div className="guest-selector">
-                            <div className="guest-selector-container">
-                                <div className="button-container">
-                                    <input className="guests-button" type="button" value="10 guests"></input>
-                                </div>
-                                <div className="down-arrow-container">
-                                    <svg className="down-arrow" viewBox="0 0 18 18" role="presentation" aria-hidden="true" focusable="false">
-                                        <path d="m16.29 4.3a1 1 0 1 1 1.41 1.42l-8 8a1 1 0 0 1 -1.41 0l-8-8a1 1 0 1 1 1.41-1.42l7.29 7.29z" >
-                                        </path>
-                                    </svg>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Guests />
 
                     <div className="reserve-container">
                         <button type="submit" className="reserve-button" >
