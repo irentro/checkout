715b6bbda83ac5cfb0648ddc20909816ea66c387
cnsarmi <christiannsarmiento@gmail.com>
2019-07-12 11:37:49 -0700

    Added checkin and checkout components. They will render when click inputs from date component

--- client/src/components/Checkout.jsx
+++ client/src/components/Checkout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Guests from './Guests.jsx'
+import Guests from './Guests.jsx';
+import Dates from './Dates.jsx';
 import axios from 'axios';
 
 class Checkout extends React.Component {
@@ -153,30 +154,7 @@ class Checkout extends React.Component {
                 </div>
 
                 <div className="booking-form">
-                    <div className="calender-component">
-                        <label className="date-label">
-                            <span className="date">Dates</span>
-                        </label>
-                        <div className="date-selector">
-                            <div className="check-in-container">
-                                <div className="checkin-input-container">
-                                    <input className="checkin-input" placeholder="Check-in">
-                                    </input>
-                                </div>
-                            </div>
-                            <div className="checkin-arrow-container" aria-hidden="true" role="presentation">
-                                <svg className="checkin-arrow"viewBox="0 0 24 24" role="presentation" aria-hidden="true" focusable="false">
-                                    <path d="m0 12.5a.5.5 0 0 0 .5.5h21.79l-6.15 6.15a.5.5 0 1 0 .71.71l7-7v-.01a.5.5 0 0 0 .14-.35.5.5 0 0 0 -.14-.35v-.01l-7-7a .5.5 0 0 0 -.71.71l6.15 6.15h-21.79a.5.5 0 0 0 -.5.5z" ></path>
-                                </svg>
-                            </div>
-                            <div className="check-out-container">
-                                <div className="checkout-input-container">
-                                    <input className="checkout-input" placeholder="Checkout">
-                                    </input>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Dates />
 
                     <Guests 
                         numGuests={this.state.numGuests} 
